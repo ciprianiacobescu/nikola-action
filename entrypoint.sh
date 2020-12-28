8930552d7037376cbce696aa12ba16a7a083318d
Ciprian <62260271+ciprianiacobescu@users.noreply.github.com>
2020-12-28 10:16:18 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,26 +8,44 @@ echo "REPO: $GITHUB_REPOSITORY"
 echo "ACTOR: $GITHUB_ACTOR"
 echo "NIKOLA_SITE: $INPUT_NIKOLA_SITE"
 
-if ! $INPUT_NIKOLA_SITE; then
-    echo "No nikola site provided"
-else
-    cd $INPUT_NIKOLA_SITE;
-    
-fi
+inanotherfolder(){
+    if ! $INPUT_NIKOLA_SITE; then
+        echo "No nikola site provided"
+    else
+        incurdir=$(pwd)
+        cd $INPUT_NIKOLA_SITE;
+        echo "now in $INPUT_NIKOLA_SITE"
+        src_branch="$(python -c 'import conf; print(conf.GITHUB_SOURCE_BRANCH)')"
+        dest_branch="$(python -c 'import conf; print(conf.GITHUB_DEPLOY_BRANCH)')"
+        
+        git remote add ghpages "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+        git fetch ghpages $dest_branch
+        git checkout -b $dest_branch --track ghpages/$dest_branch || true
+        git pull ghpages $dest_branch || true
+        git checkout $src_branch
+
+        # Override config so that ghp-import does the right thing.
+        printf '\n\nGITHUB_REMOTE_NAME = "ghpages"\nGITHUB_COMMIT_SOURCE = False\n' >> conf.py
+        
+        cd $incurdir;
+        echo "reverted to $incurdir"
+    fi
+}
 
 echo "==> Preparing..."
 if ! $INPUT_DRY_RUN; then
-    src_branch="$(python -c 'import conf; print(conf.GITHUB_SOURCE_BRANCH)')"
-    dest_branch="$(python -c 'import conf; print(conf.GITHUB_DEPLOY_BRANCH)')"
+    #src_branch="$(python -c 'import conf; print(conf.GITHUB_SOURCE_BRANCH)')"
+    #dest_branch="$(python -c 'import conf; print(conf.GITHUB_DEPLOY_BRANCH)')"
     
-    git remote add ghpages "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-    git fetch ghpages $dest_branch
-    git checkout -b $dest_branch --track ghpages/$dest_branch || true
-    git pull ghpages $dest_branch || true
-    git checkout $src_branch
+#     git remote add ghpages "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+#     git fetch ghpages $dest_branch
+#     git checkout -b $dest_branch --track ghpages/$dest_branch || true
+#     git pull ghpages $dest_branch || true
+#     git checkout $src_branch
     
     # Override config so that ghp-import does the right thing.
-    printf '\n\nGITHUB_REMOTE_NAME = "ghpages"\nGITHUB_COMMIT_SOURCE = False\n' >> conf.py
+    # printf '\n\nGITHUB_REMOTE_NAME = "ghpages"\nGITHUB_COMMIT_SOURCE = False\n' >> conf.py
+    inanotherfolder
 else
     echo "Dry-run, skipping..."
 fi
@@ -40,14 +58,25 @@ else
     pip install "Nikola[extras]"
 fi
 
-echo "==> Building site..."
-nikola build
+buildinfolder(){
 
-echo "==> Publishing..."
-if ! $INPUT_DRY_RUN; then
-    nikola github_deploy
-else
-    echo "Dry-run, skipping..."
-fi
+    if ! $INPUT_NIKOLA_SITE; then
+        echo "No nikola site provided"
+    else
+        cd $INPUT_NIKOLA_SITE;  
+    fi
+
+    echo "==> Building site..."
+    nikola build
+
+    echo "==> Publishing..."
+    if ! $INPUT_DRY_RUN; then
+        nikola github_deploy
+    else
+        echo "Dry-run, skipping..."
+    fi
+}
+
+buildinfolder
 
 echo "==> Done!"
